19a14beb549c870eae9030c77da5a87df8713f14
Fioledan <fioledan@gmail.com>
2026-03-30 02:12:13 +0400

    Add files via upload

---
+++ 1laba/TPlab1/Controllers/CalculatorController.cs
@@ -0,0 +1,89 @@
+﻿using System.Web.Mvc;
+using TPlab1.Models;
+
+namespace TPlab1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            CalculatorModel model = new CalculatorModel();
+            model.Operation = "+";
+            ViewBag.CheckValue = 10;
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(CalculatorModel model, string btn)
+        {
+            ViewBag.CheckValue = 10;
+
+            if (btn == "Очистить")
+            {
+                model.Operand1 = null;
+                model.Operand2 = null;
+                model.Operation = "+";
+                model.Result = null;
+
+                Session["OperationInfo"] = null;
+
+                ModelState.Clear();
+                return View(model);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            switch (model.Operation)
+            {
+                case "+":
+                    model.Result = (float)(model.Operand1.Value + model.Operand2.Value);
+                    break;
+                case "-":
+                    model.Result = (float)model.Operand1.Value - model.Operand2.Value;
+                    break;
+                case "*":
+                    model.Result = (float)(model.Operand1.Value * model.Operand2.Value);
+                    break;
+                case "/":
+                    model.Result = (float)model.Operand1.Value / model.Operand2.Value;
+                    break;
+            }
+
+            string operationInfo = model.Operand1 + " " + model.Operation + " " + model.Operand2 + " = " + model.Result;
+            Session["OperationInfo"] = operationInfo;
+
+            ModelState.Remove("Result");
+            return View(model);
+        }
+
+        public ActionResult Result()
+        {
+            string operationInfo = Session["OperationInfo"] as string;
+            string changedInfo = "";
+
+            if (!string.IsNullOrEmpty(operationInfo))
+            {
+                int index = operationInfo.IndexOf("=");
+
+                if (index != -1)
+                {
+                    changedInfo = operationInfo.Remove(index, 1);
+                    changedInfo = changedInfo.Insert(index, "равно");
+                }
+                else
+                {
+                    changedInfo = operationInfo;
+                }
+            }
+
+            ViewBag.OperationInfo = operationInfo;
+            ViewBag.ChangedInfo = changedInfo;
+
+            return View();
+        }
+    }
+}
---
+++ 1laba/TPlab1/Models/CalculatorModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TPlab1.Models
+{
+    public class CalculatorModel
+    {
+        [Required(ErrorMessage = "Введите первый операнд")]
+        public uint? Operand1 { get; set; }
+
+        [Compare("Operand1", ErrorMessage = "Второй операнд должен быть равен первому")]
+        public uint? Operand2 { get; set; }
+
+        public string Operation { get; set; }
+
+        public float? Result { get; set; }
+    }
+}
